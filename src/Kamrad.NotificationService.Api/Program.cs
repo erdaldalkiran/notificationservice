4163437a85d2cb8af2db8cbcc42753cc9fec4296
ed <erdal@protonmail.com>
2015-11-03 23:54:04 +0200

    #000 ed fixes log4net configuration

--- src/Kamrad.NotificationService.Api/Program.cs
+++ src/Kamrad.NotificationService.Api/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Kamrad.NotificationService.Api
+﻿using System;
+
+namespace Kamrad.NotificationService.Api
 {
     class Program
     {
+        private static NancySelfHost host = new NancySelfHost();
+
         static void Main(string[] args)
         {
+            host.Start();
+
+            Console.ReadKey();
+
+            host.Stop();
         }
     }
 }
